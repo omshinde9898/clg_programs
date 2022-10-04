8f583460b19c32c2fe8027487204e4691842d832
OmShinde <omshinde9898@gmail.com>
2022-10-04 20:29:24 +0530

    changes in practical

--- FDS/matrix.py
+++ FDS/matrix.py
@@ -34,6 +34,32 @@ def sub_smat(mat1,mat2):
         new_mat.append(r)
     return new_mat
 
+def mult(mat1,mat2):
+    new_mat = []
+    for i in range(len(mat1)):
+        r = []
+        for j in range(len(mat1[0])):
+            s =[]
+            for k in range(len(mat1)):
+                v = mat1[i][j] * mat2[k][j]
+                s.append(v)
+            r.append(sum(s))
+        new_mat.append(r)
+    return new_mat
+
+def transp(mat):
+    new_mat = []
+    for i in range(len(mat)):
+        r = []
+        for j in range(len(mat[0])):
+            r.append(0)
+        new_mat.append(r)
+    for i in range(len(mat)):
+        for j in range(len(mat[0])):
+            new_mat[i][j] = mat[j][i]
+    return new_mat
+
+
 rows = int(input("Enter Rows :"))
 cols = int(input("Enter Columns :"))
 
@@ -47,7 +73,9 @@ while True:
     print("\nChoose :")
     print("1 : Add")
     print("2 : Sub")
-    print("3 : Exit")
+    print("3 : Mult")
+    print("4 : Transpose")
+    print("5 : Exit")
     
     act = int(input(" : "))
     
@@ -58,6 +86,14 @@ while True:
         sub = sub_smat(m1,m2)
         print("Substraction : ", sub)
     elif act == 3:
+        m = mult(m1,m2)
+        print("Multiplication  : ",m)
+    elif act == 4:
+        t = transp(m1)
+        t1 = transp(m2)
+        print("Transpose of Matrix 1 : ",t)
+        print("Transpose of Matrix 2 : ",t1)
+    elif act == 5:
         exit()
     else:
         print("Choose Valid Options")
