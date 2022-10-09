f868b0b8fb50553c4b504e8e9a5ec7dcaf1ca0fd
OmShinde <omshinde9898@gmail.com>
2022-10-09 23:22:02 +0530

    Final Update

--- OOP/complex_num.cpp
+++ OOP/complex_num.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
+using namespace std;
+
 
 class complex{
     public:
@@ -37,4 +39,44 @@ complex operator* (complex c1,complex c2){
     temp.real = c1.real * c2.real - c1.img*c2.img;
     temp.img = c1.img + c2.img + c2.real*c1.img;
     return(temp);
+}
+
+int main(){
+    int ch;
+    complex c1,c2,c3;
+    cout <<"\nEnter real and Imaginary part for first complex number : ";
+    cin >> c1.real >> c1.img;
+
+    cout <<"\nEnter real and Imaginary part for second complex number : ";
+    cin >> c2.real >> c2.img;
+
+    do{
+        cout <<"\n_______ MENU ______"<<endl;
+        cout <<"\n1 : Addition"<<endl;
+        cout <<"\n2 : Substraction "<<endl;
+        cout <<"\n3 : Multiplication"<<endl;
+        cout <<"\nEnter Your Choice : "<<endl;
+        cin >> ch;
+        
+        switch (ch)
+        {
+        case 1:
+            c3 = c1+c2;
+            cout << "Addition is : "<<c3.real<<"+"<<c3.img<<"i"<<endl;
+            break;
+        case 2:
+            c3 = c1-c2;
+            cout << "Substraction is : "<<c3.real<<"+"<<c3.img<<"i"<<endl;
+            break;
+        case 3:
+            c3 = c1*c2;
+            cout << "Multiplication is : "<<c3.real<<"+"<<c3.img<<"i"<<endl;
+            break;
+        case 4:
+            exit(0);
+        default:
+            cout << "\nInvalid Choice"<<endl;
+            break;
+        }
+    }while (ch != 4);
 }
