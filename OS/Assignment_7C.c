40c6cfd7dac9237b8f63bfdc7fd1368f2be893be
OmShinde <omshinde9898@gmail.com>
2023-04-01 17:16:56 +0530

    7c

---
+++ OS/Assignment_7C.c
@@ -0,0 +1,10 @@
+//----------------------------execl-----------------------------
+#include<sys/types.h>
+#include<unistd.h>
+#include<stdio.h>
+int main()
+{
+	printf("Before execl \n");
+	execl("/bin/ls","ls",(char*)0);
+	printf("After Execl\n");
+}
